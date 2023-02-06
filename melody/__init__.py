7637e0ef3fea25dde393ffb4f6e65c22dd205396
Nikita Tikhonov <nekitdevofficial@gmail.com>
2023-02-06 21:44:34 +0300

    Add `apple`, `spotify`, `yandex` and `match` subdirectories.

---
+++ melody/__init__.py
@@ -0,0 +1,9 @@
+"""All your music, in one place."""
+
+__description__ = "All your music, in one place."
+__url__ = "https://github.com/MelodyKit/melody.web"
+
+__title__ = "melody"
+__author__ = "MelodyKit"
+__license__ = "MIT"
+__version__ = "0.1.1"
--- melody/web/__init__.py
+++ melody/web/__init__.py
@@ -1,13 +1,3 @@
-"""All your music, in one place."""
-
-__description__ = "All your music, in one place."
-__url__ = "https://github.com/MelodyKit/melody.web"
-
-__title__ = "web"
-__author__ = "MelodyKit"
-__license__ = "MIT"
-__version__ = "0.1.1"
-
 from melody.web import endpoints
 
 __all__ = ("endpoints",)
